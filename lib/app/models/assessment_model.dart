086ac4f0c677edcfb69f754ad9c9777635b81305
EDUARDO GOMES DE SA <99502227+EduardoGomesSa@users.noreply.github.com>
2023-05-16 17:12:06 -0300

    criação dos models de company e assessment

---
+++ lib/app/models/assessment_model.dart
@@ -0,0 +1,10 @@
+import 'package:avalie/app/models/company_model.dart';
+import 'package:avalie/app/models/user_model.dart';
+
+class AssessmentModel{
+  int? id;
+  UserModel? user;
+  CompanyModel? company;
+  int? note;
+  DateTime? createdAt;
+}
---
+++ lib/app/models/company_model.dart
@@ -0,0 +1,10 @@
+import 'package:avalie/app/models/assessment_model.dart';
+
+class CompanyModel {
+  String? id;
+  String? name;
+  String? branch;
+  String? city;
+  DateTime? createdAt;
+  List<AssessmentModel>? assessments;
+}
