e5ebb59369eeb99281d35a8358d5da08e75c9384
EDUARDO GOMES DE SA <99502227+EduardoGomesSa@users.noreply.github.com>
2023-05-22 23:57:20 -0300

    criação dos arquivos de info companies e form company

---
+++ lib/app/pages/home/form_company.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class FormCompanyPage extends StatelessWidget {
+  const FormCompanyPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Register of the Company'),
+      ),
+      body: Container(),
+    );
+  }
+}
---
+++ lib/app/pages/home/info_company.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class InfoCompanyPage extends StatelessWidget {
+  const InfoCompanyPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Info of the Companies'),
+      ),
+      body: Container(),
+    );
+  }
+}
