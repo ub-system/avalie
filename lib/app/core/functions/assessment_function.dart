2d774a74a68be3b7b02d9e6b39c3ed7949bbe2ba
EDUARDO GOMES DE SA <99502227+EduardoGomesSa@users.noreply.github.com>
2023-06-18 00:57:09 -0300

    mudança no retorno da função average caso a lista de assessments esteja vazia

--- lib/app/core/functions/assessment_function.dart
+++ lib/app/core/functions/assessment_function.dart
@@ -2,16 +2,16 @@ import 'package:avalie/app/models/assessment_model.dart';
 
 class AssessmentFunction{
   static double average(List<AssessmentModel> assessments) {
-  if (assessments.isEmpty) {
-    return 0.0; // Retorna 0 se o array estiver vazio para evitar divisão por zero.
-  }
+    if (assessments.isEmpty) {
+      return 1.0; // Retorna 0 se o array estiver vazio para evitar divisão por zero.
+    }
 
-  int sum = 0;
-  for (AssessmentModel assessment in assessments) {
-    sum += assessment.note!;
-  }
+    int sum = 0;
+    for (AssessmentModel assessment in assessments) {
+      sum += assessment.note!;
+    }
 
-  double average = sum / assessments.length;
-  return double.parse(average.toStringAsFixed(1));;
-}
+    double average = sum / assessments.length;
+    return double.parse(average.toStringAsFixed(1));;
+  }
 }
